96cb1f184b470a2038153ec1088c0e34dc9207b5
Evgeniy <va.evgenii@gmail.com>
2020-02-08 23:59:22 +0300

    step 3. added methods arguments \ action

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@
 const program = require('commander');
 
 program
-  .option('-V, --version", "output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .version('0.0.1', '-v, --vers', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstPath> <secondPath>')
+  .action((firstPath, secondPath) => {
+    const before = firstPath;
+    const after = secondPath;
+  });
 program.parse(process.argv);
