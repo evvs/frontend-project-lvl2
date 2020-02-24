69681112da51fa80b20ae6998f91295f43d825cd
Evgeniy <va.evgenii@gmail.com>
2020-02-24 13:55:04 +0300

    command-line interface functions
    Interface functions(methods) is created. They contain:
    - version
    - description
    - arguments (first way, second way)
    - option ('-f, --format [type]', 'output format')
    - action

---
+++ src/gendiff.js
@@ -0,0 +1,10 @@
+const commander = require('commander');
+
+const program = new commander.Command();
+
+export default () => program
+  .version('0.0.1', '-v, --vers', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstPath> <secondPath>')
+  .option('-f, --format [type]', 'output format')
+  .action(() => { });
