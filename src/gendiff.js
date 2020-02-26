9436ab208c1a962c37ceae1a2e2166880d9f670d
Evgeniy <va.evgenii@gmail.com>
2020-02-26 03:45:13 +0300

    new fuctions id added
    arguments
    action

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,10 +1,18 @@
+import genDiff from '.';
+
 const commander = require('commander');
 
 const program = new commander.Command();
 
-export default () => program
-  .version('0.0.1', '-v, --vers', 'output the version number')
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstPath> <secondPath>')
-  .option('-f, --format [type]', 'output format')
-  .action(() => { });
+export default () => {
+  program
+    .version('0.0.1', '-v, --vers', 'output the version number')
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<firstPath> <secondPath>')
+    .option('-f, --format [type]', 'output format')
+    .action((firstPath, secondPath) => {
+      const differences = genDiff(firstPath, secondPath);
+      return differences;
+    })
+    .parse(process.argv);
+};
