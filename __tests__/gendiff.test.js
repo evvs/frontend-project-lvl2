153152ffaa69b1d0bcb321b0c907d5878d2ec2f2
Evgeniy <va.evgenii@gmail.com>
2020-03-20 06:47:47 +0300

    refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,30 +4,15 @@ import render from '../src/formatters'
 import { readFile } from '../src/parsers'
 
 const getPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getExpected = (file) => readFile(getPath(file));
 
-test.each([
-  ['beforeDeep.json', 'afterDeep.json','plainRes.txt',],
-  ['beforeDeep.yml', 'afterDeep.yml','plainRes.txt',],
-  ['beforeDeep.ini', 'afterDeep.ini','plainRes.txt',],
-])('test plain', (before, after, expected) => {
-  const expectedOutput = readFile(getPath(expected));
-  expect(render(generateDifference(getPath(before), getPath(after)), 'plain')).toEqual(expectedOutput);
-})
-
-test.each([
-  ['beforeDeep.json', 'afterDeep.json','jsonRes.txt',],
-  ['beforeDeep.yml', 'afterDeep.yml','jsonRes.txt',],
-  ['beforeDeep.ini', 'afterDeep.ini','jsonRes.txt',],
-])('test json', (before, after, expected) => {
-  const expectedOutput = readFile(getPath(expected));
-  expect(render(generateDifference(getPath(before), getPath(after)), 'json')).toEqual(expectedOutput);
-})
-
-test.each([
-  ['beforeDeep.json', 'afterDeep.json','treeRes.txt',],
-  ['beforeDeep.yml', 'afterDeep.yml','treeRes.txt',],
-  ['beforeDeep.ini', 'afterDeep.ini','treeRes.txt',],
-])('test tree', (before, after, expected) => {
-  const expectedOutput = readFile(getPath(expected));
-  expect(render(generateDifference(getPath(before), getPath(after)), 'tree')).toEqual(expectedOutput);
+test('tests', () => {
+  expect(render(generateDifference(getPath('beforeDeep.json'), getPath('afterDeep.json')), 'tree'))
+  .toEqual(getExpected('treeRes.txt'));
+  expect(render(generateDifference(getPath('beforeDeep.ini'), getPath('afterDeep.ini')), 'plain'))
+  .toEqual(getExpected('plainRes.txt'));
+  expect(render(generateDifference(getPath('beforeDeep.yml'), getPath('afterDeep.yml')), 'json'))
+  .toEqual(getExpected('jsonRes.txt'));
+  expect(render(generateDifference(getPath('beforeDeep.json'), getPath('afterDeep.yml')), 'plain'))
+  .toEqual(getExpected('plainRes.txt'));
 })
