d4f256a998385b0a6984bcc31ea1fdde127bb220
Evgeniy <va.evgenii@gmail.com>
2020-04-19 21:14:38 +0300

    changed default value. changed node composition

--- src/index.js
+++ src/index.js
@@ -7,36 +7,35 @@ const path = require('path');
 
 const getPathToFile = (file) => path.resolve(process.cwd(), `${file}`);
 
+const makeNode = (type, name, oldValue = null, newValue = null, children = []) => ({
+  type, name, oldValue, newValue, children,
+});
+
+const compare = (before, after) => {
+  const keysUnion = _.union([...Object.keys(before), ...Object.keys(after)]);
+  return keysUnion.map((key) => {
+    if (_.isObject(before[key]) && _.isObject(after[key])) {
+      return makeNode('tree', key, null, null, compare(before[key], after[key]));
+    }
+    if (!_.has(after, key)) {
+      return makeNode('deleted', key, before[key]);
+    }
+    if (!_.has(before, key)) {
+      return makeNode('added', key, null, after[key]);
+    }
+    if (_.has(before, key) && _.has(after, key) && before[key] === after[key]) {
+      return makeNode('unchanged', key, before[key]);
+    }
+    if (_.has(before, key) && _.has(after, key) && before[key] !== after[key]) {
+      return makeNode('changed', key, before[key], after[key]);
+    }
+    throw new Error('Unexpected condition!');
+  });
+};
 
 const buildAst = (file1, file2) => {
   const parsedFile1 = parseFile(getPathToFile(file1));
   const parsedFile2 = parseFile(getPathToFile(file2));
-
-  const makeNode = (type, name, oldValue = null, newValue = null, children = []) => ({
-    type, name, oldValue, newValue, children,
-  });
-
-  const compare = (before, after) => {
-    const keysUnion = _.union([...Object.keys(before), ...Object.keys(after)]);
-    return keysUnion.map((key) => {
-      if (_.isObject(before[key]) && _.isObject(after[key])) {
-        return makeNode('tree', key, null, null, compare(before[key], after[key]));
-      }
-      if (!_.has(after, key)) {
-        return makeNode('deleted', key, before[key]);
-      }
-      if (!_.has(before, key)) {
-        return makeNode('added', key, null, after[key]);
-      }
-      if (_.has(before, key) && _.has(after, key) && before[key] === after[key]) {
-        return makeNode('unchanged', key, before[key]);
-      }
-      if (_.has(before, key) && _.has(after, key) && before[key] !== after[key]) {
-        return makeNode('changed', key, before[key], after[key]);
-      }
-      return null;
-    });
-  };
 
   return compare(parsedFile1, parsedFile2);
 };
