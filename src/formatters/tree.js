6b218ba340ef229d8d45df13a375f8a861af5f39
Evgeniy <va.evgenii@gmail.com>
2020-04-19 21:13:29 +0300

    changed default value

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -18,29 +18,29 @@ const stringify = (nodeValue, depth) => {
 const render = (tree) => {
   const iter = (nodes, depth) => {
     const renderString = nodes.reduce((acc, node) => {
-      switch (node.action) {
+      switch (node.type) {
         case 'added':
-          return `${acc}${indent(depth)}+ ${node.name}: ${stringify(node.value, depth)}\n`;
+          return `${acc}${indent(depth)}+ ${node.name}: ${stringify(node.newValue, depth)}\n`;
 
         case 'unchanged':
-          return _.has(node, 'children') ? `${acc}  ${indent(depth)}${node.name}: {
-${iter(node.children, depth + 1)}${indent(depth)}  }\n`
-            : `${acc}${indent(depth)}  ${node.name}: ${stringify(node.value, depth)}\n`;
+          return `${acc}${indent(depth)}  ${node.name}: ${stringify(node.oldValue, depth)}\n`;
+
+        case 'tree':
+          return `${acc}  ${indent(depth)}${node.name}: {
+${iter(node.children, depth + 1)}${indent(depth)}  }\n`;
 
         case 'deleted':
-          return `${acc}${indent(depth)}- ${node.name}: ${stringify(node.value, depth)}\n`;
+          return `${acc}${indent(depth)}- ${node.name}: ${stringify(node.oldValue, depth)}\n`;
 
         case 'changed':
           return `${acc}${indent(depth)}- ${node.name}: ${stringify(node.oldValue, depth)}
 ${indent(depth)}+ ${node.name}: ${stringify(node.newValue, depth)}\n`;
         default:
-          return null;
+          throw new Error(`Unknown node type: '${node.type}'!`);
       }
     }, '');
-
     return renderString;
   };
-
   return `{\n  ${iter(tree, 0).trim()}\n}`;
 };
 
