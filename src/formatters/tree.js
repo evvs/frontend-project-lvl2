a1626be120c4298d57eeaf7301aff812bc52832e
Evgeniy <va.evgenii@gmail.com>
2020-04-23 23:09:29 +0300

    code refactoring

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -15,32 +15,25 @@ const stringify = (nodeValue, depth) => {
   return ['{', result, `${indent(depth)}  }`].join('\n');
 };
 
-const render = (tree) => {
-  const iter = (nodes, depth) => {
-    const renderString = nodes.reduce((acc, node) => {
-      switch (node.type) {
-        case 'added':
-          return `${acc}${indent(depth)}+ ${node.name}: ${stringify(node.newValue, depth)}\n`;
+const generateOutput = (node, acc, depth, iter) => {
+  const { type } = node;
 
-        case 'unchanged':
-          return `${acc}${indent(depth)}  ${node.name}: ${stringify(node.oldValue, depth)}\n`;
-
-        case 'tree':
-          return `${acc}  ${indent(depth)}${node.name}: {\n${iter(node.children, depth + 1)}${indent(depth)}  }\n`;
-
-        case 'deleted':
-          return `${acc}${indent(depth)}- ${node.name}: ${stringify(node.oldValue, depth)}\n`;
-
-        case 'changed':
-          return `${acc}${indent(depth)}- ${node.name}: ${stringify(node.oldValue, depth)}\n${indent(depth)}+ ${node.name}: ${stringify(node.newValue, depth)}\n`;
-        default:
-          throw new Error(`Unknown node type: '${node.type}'!`);
-      }
-    }, '');
-
-    return renderString;
+  const nodeTypes = {
+    tree: `${acc}  ${indent(depth)}${node.name}: {\n${iter(node.children, depth + 1)}${indent(depth)}  }\n`,
+    added: `${acc}${indent(depth)}+ ${node.name}: ${stringify(node.newValue, depth)}\n`,
+    deleted: `${acc}${indent(depth)}- ${node.name}: ${stringify(node.oldValue, depth)}\n`,
+    changed: `${acc}${indent(depth)}- ${node.name}: ${stringify(node.oldValue, depth)}\n${indent(depth)}+ ${node.name}: ${stringify(node.newValue, depth)}\n`,
+    unchanged: `${acc}${indent(depth)}  ${node.name}: ${stringify(node.oldValue, depth)}\n`,
   };
-  return `{\n  ${iter(tree, 0).trim()}\n}`;
+
+  return nodeTypes[type];
+};
+
+
+const render = (tree) => {
+  const iter = (nodes, depth) => nodes.reduce((acc, node) => generateOutput(node, acc, depth, iter), '');
+  const result = `{\n  ${iter(tree, 0).trim()}\n}`;
+  return result;
 };
 
 export default render;
